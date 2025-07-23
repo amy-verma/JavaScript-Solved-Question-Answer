6e8927817579b0f14d1dfde2ab3a866e5dfbf551
Rishabh Kumar <rishabh2807s@gmail.com>
2025-07-23 23:30:14 +0530

    polyfill-map

--- polufill-map-filter-reduce.js
+++ polufill-map-filter-reduce.js
@@ -1,20 +1,41 @@
+// Polyfil for filter
+
 // let arr = [2, 3, 4];
 // let res = arr.reduce((acc, curr) => {
 //   return acc + curr;
 // }, 0);
 // console.log(res);
 
-Array.prototype.myArray = function (callback, initialValue) {
-  let accumulator = initialValue;
+// Array.prototype.myArray = function (callback, initialValue) {
+//   let accumulator = initialValue;
 
-  for (let i = 0; i < this.length; i++) {
-    if (accumulator) {
-      accumulator = callback.call(undefined, accumulator, this[i], this);
-    } else {
-      accumulator = this[i];
-    }
-  }
-  return accumulator;
-};
-let arr = [1, 3, 4, 5];
-console.log(arr.myArray((acc, curr) => acc + curr));
+//   for (let i = 0; i < this.length; i++) {
+//     if (accumulator) {
+//       accumulator = callback.call(undefined, accumulator, this[i], this);
+//     } else {
+//       accumulator = this[i];
+//     }
+//   }
+//   return accumulator;
+// };
+// let arr = [1, 3, 4, 5];
+// console.log(arr.myArray((acc, curr) => acc + curr));
+
+//////////////map - Polyfil////////////////////////////////////////////
+
+// const arr=[2,3,4]
+// const res=arr.map((ele)=>ele*2)
+// console.log(res)
+
+// Array.prototype.myArray = function (callback) {
+//   let result = [];
+//   if (typeof myArray === "function") {
+//     throw Error(`${callback} is not an array`);
+//   }
+//   for (let i = 0; i < this.length; i++) {
+//     result.push(callback(this[i], i, this));
+//   }
+//   return result;
+// };
+// let arr = [2, 3, 4];
+// console.log(arr.myArray((ele) => ele * 2));
