6a78e98ecf21bf9bd2b2be7e262e97b6b959579c
amy-verma <amitjsr8786@gmail.com>
2025-09-05 00:12:47 +0530

    const-example

--- hoisting.js
+++ hoisting.js
@@ -8,3 +8,11 @@ var x=10;
 
 //Hoisting is a phenomenon is which variable and function decleration are moved to top of their containing
 // scope during the execution phase before the code is actually run
+
+// few Examples on Var let and const
+
+const name = 'Lydia';
+age = 21;
+
+console.log(delete name);//false
+console.log(delete age);//true
