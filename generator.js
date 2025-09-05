410f87084b66c1dadcfbab35c5700f998b951a57
amy-verma <amitjsr8786@gmail.com>
2025-09-05 13:17:17 +0530

    generato function

--- generator.js
+++ generator.js
@@ -13,3 +13,15 @@ console.log(res.next());
 console.log(res.next());
 console.log(res.next());
 console.log(res.next());
+
+// Q. 1. What is a generator function in JavaScript?
+// - A generator function is a special type of function that can be paused and resumed during its execution.
+// Unlike normal functions that run to completion, generator functions yield values one at a time using the
+//  yield keyword.
+
+// 3. What does the yield keyword do inside a generator function?
+// - The yield keyword pauses the generator and returns a value to the caller.
+// The function execution can later be resumed from the point it was paused using .next().
+
+// 5. What does the .next() method do on a generator object?
+// It resumes execution of the generator function and returns an object:
